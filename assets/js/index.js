a91375ef873f4f23f5e7661223378ab27e58264a
ely211 <gonzalesm405@gmail.com>
2017-07-08 06:56:06 -0500

    menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
   //menu
-  $(".button-collapse").sideNav();
+  $(".button-collapse").sideNav({
+    menuWidth: 200,
+    closeOnClick: true
+  });
   $('#log-in').click(()=>{
     $('#log-in-container').toggle('waves-block');
   });
-  $('.carousel.carousel-slider').carousel({fullWidth: true})
+  $('.carousel.carousel-slider').carousel({fullWidth: true, duration: 1000});
   $('.parallax').parallax();
   $('.modal').modal();
 });
