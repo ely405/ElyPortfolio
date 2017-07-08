61b09eb39dc0d460d1049490b8e06e76daec6a34
ely211 <gonzalesm405@gmail.com>
2017-07-08 07:09:22 -0500

    modal

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
   $('#log-in').click(()=>{
     $('#log-in-container').toggle('waves-block');
   });
-  $('.carousel.carousel-slider').carousel({fullWidth: true, duration: 1000});
   $('.parallax').parallax();
   $('.modal').modal();
+  $('.slider').slider();
 });
