3c1d9de8c20f146f5c0df6baf5e10ed5b3ede0b9
ely405 <gonzalesm405@gmail.com>
2018-03-17 16:30:49 -0500

    cambio en el menu, iconos

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
   //menu
   $(".button-collapse").sideNav({
-    menuWidth: 200,
+    menuWidth: '30%',
     closeOnClick: true
   });
-  $('#log-in').click(()=>{
-    $('#log-in-container').toggle('waves-block');
-  });
+  // $('#log-in').click(()=>{
+  //   $('#log-in-container').toggle('waves-block');
+  // });
   $('.parallax').parallax();
   $('.modal').modal();
   $('.slider').slider();
