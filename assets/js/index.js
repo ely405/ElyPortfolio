9a65653ce182ae16131112729c15c4d610eeefa7
ely405 <gonzalesm405@gmail.com>
2018-03-28 16:30:04 -0500

    agregando preloader

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,32 @@
-const showAndHideSocialNetworks = () => {
-    $('#socialNetworksFixed').hide();
-    
-    $(window).scroll(() => {
-        var windowHeight = $(window).scrollTop();
-        var contactSection = $("#contact-section").offset();
-        contactSection = contactSection.top;
-
-        if(windowHeight >= contactSection){
-            $('#socialNetworksFixed').fadeOut(500);
-        }else{
-            $('#socialNetworksFixed').fadeIn(500);
-        }
-    });
+window.onload = function(e){ 
+	$('#page-loader').fadeOut(1000);
+	$('#page-loader').css('background', 'black');
 }
 
-$(document).ready(function(){
-    //menu
-    $(".button-collapse").sideNav({
-        menuWidth: 200,
-        closeOnClick: true
-    });
-    $('.parallax').parallax();
 
-    showAndHideSocialNetworks();
+const showAndHideSocialNetworks = () => {
+	$('#socialNetworksFixed').hide();
+
+	$(window).scroll(() => {
+		var windowHeight = $(window).scrollTop();
+		var contactSection = $("#contact-section").offset();
+		contactSection = contactSection.top;
+
+		if (windowHeight >= contactSection) {
+			$('#socialNetworksFixed').fadeOut(500);
+		} else {
+			$('#socialNetworksFixed').fadeIn(500);
+		}
+	});
+}
+
+$(document).ready(function () {
+	//menu
+	$(".button-collapse").sideNav({
+		menuWidth: 200,
+		closeOnClick: true
+	});
+	$('.parallax').parallax();
+
+	showAndHideSocialNetworks();
 });
